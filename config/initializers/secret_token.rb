86b00f64645abf7398118523b71682bf984b445a
geckoplus <pavlifestyle@gmail.com>
2014-01-29 22:19:35 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,28 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SmapleApp::Application.config.secret_key_base = 'd07a7bd1b073d2351e3a5b92a935fe1a2aeccf056601c0bd9a27ffe38e1a8339e5684065fca88c8c58f8ec77ce03da17c945755bb18be886c05cf051168042a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmapleApp::Application.config.session_store :cookie_store, key: '_smaple_app_session'
